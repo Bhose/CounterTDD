dd6225d558d84670a67f359810899d1f8304016e
Brittney Hose <bhose10@gmail.com>
2017-11-26 09:38:25 -0500

    divisible by 5 counter

--- counter.rb
+++ counter.rb
@@ -7,3 +7,18 @@ end
 def counterClass (test_number)
 	test_number.class 
 end
+
+def counting (test_number)
+	10.times do
+		$counterAr.push(test_number)
+		test_number += test_number
+end
+puts $counterAr
+	if $counterAr.last % 5 == 0
+		true
+	else 
+		false
+	end
+
+		
+	end
